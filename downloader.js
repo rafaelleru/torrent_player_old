7e336c2b00d34c89fd3a5a94b5e1dc922b0269e5
rafa <rafaelleru95103@gmail.com>
2016-10-08 21:39:40 +0200

    pequeñas modificaciones para que muestre la lista de canciones de un torrent

--- downloader.js
+++ downloader.js
@@ -1,14 +1,41 @@
 var WebTorrent = require('webtorrent')
 
 var client = new WebTorrent()
+var Document 
 
-var torrentId = 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
+var torrentId = 'magnet:?xt=urn:btih:1127fcb8e3a7952d236b257ed75cc49e5d9fa919&dn=Dream+Theater+-+The+Number+Of+The+Beast+2005+320ak&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969'
 
 client.add(torrentId, function(torrent) {
-console.log('Holaaaaaaaaaaaaaaaaa')
     // Torrents can contain many files. Let's use the first.
-var file = torrent.files[0]
+    var files = torrent.files
+    rellenarLista(files)
+    
+    console.log(files.length)
+    for (var i = 0; i < files.length; i++) {
+	console.log(files[i].name)
+	/*while (client.downloaded < files[i].length){
+	    
+	}*/
+    }
 
-// Display the file by adding it to the DOM. Supports video, audio, image, etc. files
-file.appendTo('body')
+    playSelected(files);
 })
+
+function rellenarLista(files) {
+    lista = ['http://www.ksdjfksdjgfsjdhfsdf.com','http://www.manolico.com','http://www.laputaqueteparioquefuetumaeynotieneculpa.com'];
+                var bloque = document.getElementById("lista");
+                for (var i = 0; i < files.length; i++) {
+                    var texto = document.createElement("li");
+		    texto.id = i;
+		    //console.log(files[i])
+                    texto.innerHTML = files[i].name;
+                    bloque.appendChild(texto);
+                }
+}
+
+function playSelected(files){
+    var index = document.getElementById('5');
+    file = files[5];
+    file.appendTo('body');
+}
+    
