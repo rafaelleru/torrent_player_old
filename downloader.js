84e70f426c1326adc16f5b047f56b67029f7f0e7
rafa <rafaelleru95103@gmail.com>
2016-10-17 16:42:02 +0200

    la lista de archivos ya no se duplica, menos problemas

--- downloader.js
+++ downloader.js
@@ -1,19 +1,19 @@
 var WebTorrent = require('webtorrent')
 var client = new WebTorrent()
-var files
-
+var files = []
+var reproduciendo
 var torrentId //= 'magnet:?xt=urn:btih:1127fcb8e3a7952d236b257ed75cc49e5d9fa919&dn=Dream+Theater+-+The+Number+Of+The+Beast+2005+320ak&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969'
 
 module.exports={ 
     addTorrent:function addTorrent(torrentID){
 	client.add(torrentID, function(torrent) {
 	// Torrents can contain many files. Let's use the first.
-	    files = torrent.files;
+	    //files = torrent.files;
 	    update();
 
 	    
 	
-	    var reproduciendo = files[0].appendTo("body");
+	    //var reproduciendo = files[0].appendTo("body");
 	//var click = listenClick(files);
 	/*if(click != null){
 	  console.log("voy a reproducir: " + click.toString());
@@ -26,17 +26,17 @@ module.exports={
  * muestra una lista de las canciones
 * @param files archivos del torrent
 */
-    function rellenarLista(files) {
-	var bloque = document.getElementById("songs_queue");
-	for (var i = 0; i < files.length; i++) {
-            var texto = document.createElement("li");
-	    //le asignamos un id a cada elemento de la lista para referenciarlos luego
-	    texto.setAttribute("id", "item_"+i.toString());
-	    //console.log(files[i])
-            texto.innerHTML = files[i].name;
-            bloque.appendChild(texto);
-	}
+function rellenarLista(files) {
+    var bloque = document.getElementById("songs_queue");
+    for (var i = 0; i < files.length; i++) {
+        var texto = document.createElement("li");
+	//le asignamos un id a cada elemento de la lista para referenciarlos luego
+	texto.setAttribute("id", "item_"+i.toString());
+	//console.log(files[i])
+        texto.innerHTML = files[i].name;
+        bloque.appendChild(texto);
     }
+}
 
 /**
 * funcion que comprueba si hay click en algun elemento de la lista de canciones
@@ -84,6 +84,10 @@ function update(){
 	    files.push(f);
 	})
     })
+
+    if(reproduciendo == null){
+	reproduciendo = files[3].appendTo('body')
+    }
     
     //files = client.getTorrent(1).files; 
     rellenarLista(files);
