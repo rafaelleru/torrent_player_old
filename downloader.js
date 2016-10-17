3de84eb518dbd549d8f43b8d19c13cff5f6e927b
rafa <rafaelleru95103@gmail.com>
2016-10-17 16:33:58 +0200

    añadido module export, para añadir torrents desde otro lao, cambiada funcion rellenar listase raya reproduciendo

--- downloader.js
+++ downloader.js
@@ -1,45 +1,42 @@
 var WebTorrent = require('webtorrent')
 var client = new WebTorrent()
-var dragDrop = require('drag-drop/buffer')
 var files
 
 var torrentId //= 'magnet:?xt=urn:btih:1127fcb8e3a7952d236b257ed75cc49e5d9fa919&dn=Dream+Theater+-+The+Number+Of+The+Beast+2005+320ak&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969'
 
-dragDrop(document.getElementById("draganddrop"), function(file){
-    //console.log(file);
-    torrentID = file;
-    addTorrent(torrentID);
-})
+module.exports={ 
+    addTorrent:function addTorrent(torrentID){
+	client.add(torrentID, function(torrent) {
+	// Torrents can contain many files. Let's use the first.
+	    files = torrent.files;
+	    update();
 
-function addTorrent(torrentID){
-client.add(torrentID, function(torrent) {
-    // Torrents can contain many files. Let's use the first.
-    files = torrent.files;
-    update();
+	    
 	
-    var reproduciendo = files[0].appendTo("body");
-    var click = listenClick(files);
+	    var reproduciendo = files[0].appendTo("body");
+	//var click = listenClick(files);
 	/*if(click != null){
 	  console.log("voy a reproducir: " + click.toString());
 	  files[click].appendTo("body");
 	  }*/
-})
-}
-/**
-* muestra una lista de las canciones
-* @param files archivos del torrent
-*/
-function rellenarLista(files) {
-    var bloque = document.getElementById("songs_queue");
-    for (var i = 0; i < files.length; i++) {
-        var texto = document.createElement("li");
-	//le asignamos un id a cada elemento de la lista para referenciarlos luego
-	texto.setAttribute("id", "item_"+i.toString());
-	//console.log(files[i])
-        texto.innerHTML = files[i].name;
-        bloque.appendChild(texto);
+	})
     }
 }
+/**
+ * muestra una lista de las canciones
+* @param files archivos del torrent
+*/
+    function rellenarLista(files) {
+	var bloque = document.getElementById("songs_queue");
+	for (var i = 0; i < files.length; i++) {
+            var texto = document.createElement("li");
+	    //le asignamos un id a cada elemento de la lista para referenciarlos luego
+	    texto.setAttribute("id", "item_"+i.toString());
+	    //console.log(files[i])
+            texto.innerHTML = files[i].name;
+            bloque.appendChild(texto);
+	}
+    }
 
 /**
 * funcion que comprueba si hay click en algun elemento de la lista de canciones
@@ -51,13 +48,17 @@ function listenClick(files){
 	var clicada = getEventTarget(e);
 	var index = clicada.id;
 	var num_str = index.replace("item_", "");
-	console.log(num_str);
+	/*console.log(num_str);
+	
+	console.log("reproduciendo: " + files[num].name);*/
 	var num = parseInt(num_str);
-	console.log("reproduciendo: " + files[num].name);
 	var el = document.querySelector('audio');
-	console.log(el);
-	el.parentNode.removeChild(el)
-	files[num].appendTo("body");
+	if(el != null){
+	    console.log(el);
+	    el.parentNode.removeChild(el);
+	}
+
+	files[num].appendTo('body');
 	//var num = parseInt(index);
 	//console.log(num.toString());
 	//alert(clicada.innerHTML);
