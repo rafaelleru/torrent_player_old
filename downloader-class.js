5adbd1dc6e06ea15d0a1b1bc91cb3bc4de0fcf35
rafa <rafaelleru95103@gmail.com>
2016-10-21 14:00:22 +0200

    primeros metodos de la clase downloader.

--- downloader-class.js
+++ downloader-class.js
@@ -5,7 +5,25 @@ var downloader = Downloader.prototype;
 
 function Downloader(_torrent){
     this.torrent = _torrent;
-    this.client.add(this.torrent, function(_torrent) {
-	actualizaLista();
+}
+
+Downloader.startDownload() = function() {
+    this.client = client.add( this.torrent, function(){
+	//nada de momento
     })
 }
+
+
+Downloader.setFileMorePriority = function(file) {
+    this.client.files[file].select();
+}
+
+Downloader.setFileBuffer = function(file) {
+    stream  = this.client.files[file].getBuffer();
+    //poner a reproducir el buffer
+}
+
+Downloader.setPlayFile = function(file) {
+    document.getElementById("body").append(this.client.files[file]);
+}
+
