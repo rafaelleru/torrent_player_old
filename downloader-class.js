d9be6932b9667c8c0a2ad43fc49d9c28ebbc59ed
rafa <rafaelleru95103@gmail.com>
2016-10-21 18:34:38 +0200

    añadido getFiles, solucionado error client undefined.

--- downloader-class.js
+++ downloader-class.js
@@ -4,6 +4,7 @@
 function Downloader(){
     console.log('Holaaaa');
     this._torrentsArray = [];
+    this.client = new WebTorrent
 };
 
 Downloader.prototype.startDownload = function(torrent) {
@@ -32,4 +33,12 @@ Downloader.prototype.setPlayFile = function(file) {
     document.getElementById("body").append(this.client.files[file]);
 };
 
+Downloader.prototype.getFiles = function(){
+    var files = [];
+    client.torrents.files.forEach( function(f){
+	files.push(f);
+    })
+
+    return files;
+}
 module.exports = Downloader;
