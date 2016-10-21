8822dfd3e0c90edf65b110c2c570151a043706c7
rafa <rafaelleru95103@gmail.com>
2016-10-21 16:48:59 +0200

    Cambiado prototype a cosas mas estandar, sigue con la misma excepcion

--- downloader-class.js
+++ downloader-class.js
@@ -1,39 +1,36 @@
 //clase method
 //author @rafaelleru
 
-var Downloader = Downloader.prototype;
-
 function Downloader(){
     console.log('Holaaaa');
     this._torrentsArray = [];
 };
 
-Downloader.startDownload() = function() {
+Downloader.prototype.startDownload() = function() {
     console.log("hola");
     this.client = client.add( this.torrent, function(){
 	console.log("algoo");
     });
 };
 
-Downloader.addTorrent(torrent) = function(torrent){
+Downloader.prototype.addTorrent(torrent) = function(torrent){
     this.torrentsArray.push(torrent);
     if(torrentsArray.length != 0){
 	this.client.add(this.torrentsArray[this.torrentsArray.length]);
     };
 };
 
-Downloader.setFileMorePriority = function(file) {
+Downloader.prototype.setFileMorePriority = function(file) {
     this.client.files[file].select();
 };
 
-Downloader.setFileBuffer = function(file) {
+Downloader.prototype.setFileBuffer = function(file) {
     stream  = this.client.files[file].getBuffer();
     //poner a reproducir el buffer
 };
 
-Downloader.setPlayFile = function(file) {
+Downloader.prortype.setPlayFile = function(file) {
     document.getElementById("body").append(this.client.files[file]);
 };
 
 module.exports = Downloader;
-exports.Downloader = Downloader;
