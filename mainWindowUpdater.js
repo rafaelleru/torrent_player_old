62524b30937de150b0aeb33b2f61d965c8d09377
rafa <rafaelleru95103@gmail.com>
2016-10-22 13:41:03 +0200

    fixed, closes #28

--- mainWindowUpdater.js
+++ mainWindowUpdater.js
@@ -7,11 +7,11 @@ function Updater(){
 
 Updater.prototype.reloadList = function(files){
     for(var i=0; i < files.length; i++){
-	var list_element = documente.createElement('li');
-	texto.setAttribute('id', 'item_'+ i.toString());
+	var list_element = document.createElement('li');
+	list_element.setAttribute('id', 'item_'+ i.toString());
 
 	if(files[i].name.includes('mp3',files[i].name.length - 4)){
-	    list_element.innerHTML(files[i].name);
+	    list_element.innerHTML = files[i].name;
 	    this.list_div.appendChild(list_element);
 	}
     }
