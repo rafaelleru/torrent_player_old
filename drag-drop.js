b247dd9e45384c0a033d33b31e27ad5711b73d15
Sergio <sergio_basket93@hotmail.com>
2016-10-27 12:05:06 +0200

    Solucionados fallos en index.html y dragrop

--- drag-drop.js
+++ drag-drop.js
@@ -1,7 +1,7 @@
 var dragDrop = require('drag-drop/buffer')
 var ipc = require('electron').ipcRenderer;
 
-dragDrop(document.getElementById("draganddrop"), function(files){
+dragDrop(document.getElementById("body"), function(files){
     ipc.send('addTorrent', files);
     
 })
