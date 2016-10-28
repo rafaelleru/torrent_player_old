44a94065b84238070a239c5c30847d3906bccb85
rafa <rafaelleru95103@gmail.com>
2016-10-28 20:33:58 +0200

    log y envio del mensaje de solicitud de archivo para reproducir

--- mainWindowUpdater.js
+++ mainWindowUpdater.js
@@ -8,9 +8,9 @@ function Updater(){
 };
 
 Updater.prototype.reloadList = function(files, n_torrent){
-    for(var i=0; i < files.length; i++){
+    console.log(files.length.toString() + ' archivos del torrent: '+ n_torrent.toString());
 
-        console.log(i);
+    for(var i=0; i < files.length; i++){
         var list_element = document.createElement('li');
         list_element.setAttribute('id', 'item_'+ i.toString() + '_' + n_torrent.toString());
 
@@ -23,7 +23,7 @@ Updater.prototype.reloadList = function(files, n_torrent){
 	list_element.setAttribute('id', 'item_'+ i.toString());
 	(function (i){
 	    list_element.onclick = function(){
-		requestPlay(i, n_torrent);
+		requestPlay(i, n_torrent-1);
 	    };
 	})(i);
 
@@ -32,8 +32,8 @@ Updater.prototype.reloadList = function(files, n_torrent){
 // TODO: voy a poner el click listener aqui, pero hay que pensar mejor donde ponerlo
 
 function requestPlay(i, n_torrent){
-    ipcRenderer.send('playRequest', i);
-    ipcRenderer.on('toPlay', (event, file, n_torrent) => {
+    ipcRenderer.send('playRequest', [i, n_torrent]);
+    ipcRenderer.on('toPlay', (event, file) => {
 //	console.log('voy a intentar reproducir a ver que pasa');
 //	console.log(file.typeof);
 	file.appendTo('body');
