a845bf5504384008ab49a53b3012c30ec8fb4ff2
rafa <rafaelleru95103@gmail.com>
2016-10-28 18:24:46 +0200

    merged rafa_development

--- mainWindowUpdater.js
+++ mainWindowUpdater.js
@@ -4,29 +4,27 @@ const {ipcRenderer} = require('electron')
 
 function Updater(){
     this.list = [];
+    this.list_div = document.getElementById('songs_queue');
 };
 
 Updater.prototype.reloadList = function(files){
-    for(var i=0; i < files.length; i++){ 
-	//console.log(i);
-	console.log(files[i].name);
-	var list_element = document.createElement('li');
+    for(var i=0; i < files.length; i++){
+
+        console.log(i);
+        var list_element = document.createElement('li');
         list_element.setAttribute('id', 'item_'+ i.toString());
 
         //if(files[i].name.includes('mp3',files[i].name.length - 4)){
-	    console.log(files[i].name);
             list_element.innerHTML=files[i].name;
             this.list_div.appendChild(list_element);
         //}
 
-	(function(i){
-	    list_element.onclick = function () {
-		requestPlay(i);
-	    }
-	})(i);
+	var list_element = document.createElement('li');
+	list_element.setAttribute('id', 'item_'+ i.toString());
+	list_element.onclick = requestPlay(i);
+
     }
 };
-
 // TODO: voy a poner el click listener aqui, pero hay que pensar mejor donde ponerlo
 
 function requestPlay(i){
