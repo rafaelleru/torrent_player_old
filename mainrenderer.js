2b38170816804427d3aded09f5edf9439d18c674
rafa <rafaelleru95103@gmail.com>
2016-11-04 10:51:10 +0100

    añadido main renderer

---
+++ mainrenderer.js
@@ -0,0 +1,18 @@
+const ipc = require('electron').ipcRenderer;
+var webtorrent = require('webtorrent');
+var Updater = require('./mainWindowUpdater.js');
+var updater = new Updater();
+
+ipc.on('updatePlayList', (event, data) => {
+    updater.reloadList(data[0], data[1]);
+});
+
+ipc.on('toPlay', (event, data) => {
+    body = document.getElementById('body');
+    audio = document.getElementById('audio');
+    console.log(data[0]);
+    audio.src = '/tmp/webtorrent/' + data[0] + '/' + data[1];
+    //body.appendChild(audio);
+    audio.load();
+    audio.play();
+})
