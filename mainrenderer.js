c159fb2ee854c8be47884bcb50a44c719e7c1f7f
rafa <rafaelleru95103@gmail.com>
2016-11-08 10:28:39 +0100

    intentando que funcione el streaming realmente

--- mainrenderer.js
+++ mainrenderer.js
@@ -2,26 +2,41 @@ const ipc = require('electron').ipcRenderer;
 var webtorrent = require('webtorrent');
 var Updater = require('./mainWindowUpdater.js');
 var updater = new Updater();
-var fs = require('fs');
+var render = require('render-media')
+var Readable = require('stream').Readable;
+var stream_data = new Readable();
 
 ipc.on('updatePlayList', (event, data) => {
     updater.reloadList(data[0], data[1]);
 });
 
-ipc.on('toPlay', (event, data) => {
-    body = document.getElementById('body');
-    audio = document.getElementById('audio');
-    console.log(data[0]);
-    audio.src = '/tmp/webtorrent/' + data[0] + '/' + data[1];
-    audio.
-    //body.appendChild(audio);
-    audio.play();
+ipc.on('addData', (event, data) => {
+    stream_data.push(data, 'utf8');
 })
+
+ipc.on('toPlay', (event, data) => {
+    console.log('toPlay: ' + data[0]);
+    var file = {
+	name: data[0],
+	createReadStream: function(){
+	    return stream_data;
+	}
+    }
+    
+    render.append(file, 'body', function(err, elem){
+	if(err)
+	    return console.log('error appending file');
+
+	return console.log(elem);
+    });
+})
+
+
 
 ipc.on('updateProgress', (event, data) => { 
     progressBar = document.getElementById('progress-bar');
-    progressBar.style.width = data[2][0]+"%";
-    console.log(data[2][0]);
+    progressBar.style.width = data+"%";
+    console.log(data);
     audio = document.getElementById('audio');
     audio.style.display = 'block';
 })
