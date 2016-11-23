daecbde00d3f2ceb48d553e717a41fdbb42d0bcc
rafa <rafaelleru95103@gmail.com>
2016-11-23 19:10:53 +0100

    autoplay

--- src/songsManager.js
+++ src/songsManager.js
@@ -3,23 +3,25 @@ const ipc = require('electron').ipcRenderer;
 var webtorrent = require('webtorrent');
 var Updater = require('./src/mainWindowUpdater.js');
 var updater = new Updater();
-
+var audio_tag = document.getElementById('audio');
+var currentPlay;
 
 ipc.on('updatePlayList', (event, data) => {
     updater.reloadList(data[0], data[1]);
 });
 
 ipc.on('toPlay', (event, data) => {
-
-    var buf = [];
-    var length = parseInt(data[1]);
-    var stream_data = new Buffer(parseInt(data[1]));
-
-    
+  
     console.log(data.toString());
     
-    var audio_tag = document.getElementById('audio');
     audio_tag.src = 'http://localhost:9999/' + data.toString();
+    audio_tag.play();
+
+    audio_tag.onended = function(){
+	console.log('se acabo la reproduccion');
+	ipc.send('playEnded', []);
+    }
+
 })
 
 
@@ -31,3 +33,4 @@ ipc.on('updateProgress', (event, data) => {
     audio = document.getElementById('audio');
     audio.style.display = 'block';
 })
+
