f383a7477437f22fae24e708a96cdf7a82e05cbb
Rafael <rafaelleru95103@gmail.com>
2017-03-15 18:46:34 +0100

    passing

--- test/index.js
+++ test/index.js
@@ -4,22 +4,22 @@ var assert = require('assert')
 describe('application launch', function () {
   this.timeout(10000)
 
-  beforeEach(function () {
-    this.app = new Application({
-      path: __dirname + '/../Torrent-Player-linux-x64/Torrent-Player'
+    beforeEach(function () {
+	this.app = new Application({
+	    path: __dirname + '/../Torrent-Player-linux-x64/Torrent-Player'
+	})
+	return this.app.start()
     })
-    return this.app.start()
-  })
 
-  afterEach(function () {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop()
-    }
-  })
-
-  it('shows an initial window', function () {
-    return this.app.client.getWindowCount().then(function (count) {
-      assert.equal(count, 1)
+    afterEach(function () {
+	if (this.app && this.app.isRunning()) {
+	    return this.app.stop()
+	}
     })
-  })
+
+    it('shows an initial window', function () {
+	return this.app.client.getWindowCount().then(function (count) {
+	    assert.equal(count, 1)
+	})
+    })
 })
