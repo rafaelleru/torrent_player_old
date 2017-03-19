a7bc412aa5c3442d9226d169e1cfb67c57b4326d
Rafael <rafaelleru95103@gmail.com>
2017-03-19 23:04:23 +0100

    cosas

--- Torrent-Player-linux-x64/resources/app/src/mainPageLoader.js
+++
@@ -1,5 +0,0 @@
-var songs = require("./songsManager.js")
-var main_songs_file = "~/.config/torrentPlayer/mainList"
-
-var latestDiscs = [];
-
--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ function createWindow() {
     })
 
     // and load the index.html of the app.
-    mainWindow.loadURL(`file://${__dirname}/../index.html`)
+    mainWindow.loadURL(`file://${__dirname}/../index2.html`)
 
 
 
