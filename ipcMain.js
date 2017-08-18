943510f82cc2232a9f3bb0bf8e04f8a4ebfe05d3
Rafael <rafaelleru95103@gmail.com>
2017-08-19 00:55:52 +0200

    async each to use duration

--- ipcMain.js
+++ ipcMain.js
@@ -1,19 +1,30 @@
 const {ipcMain} = require('electron')
 const WebTorrent = require('webtorrent')
 const client = new WebTorrent()
-var server
+const mp3Duration = require('mp3-duration')
+const async = require('async')
+
+var server;
 
 ipcMain.on('add-torrent', (event, arg) => {
   client.add(arg, function(torrent) {
     var files = []
-    torrent.files.forEach(function(f) {
-      files.push({
-        title: f.name,
-        torrent: torrent.infoHash,
-        index: torrent.files.indexOf(f)
+    async.each(torrent.files, function(f, callback) {
+      mp3Duration('/tmp/webtorrent/' + torrent.infoHash + '/' + f.path, function(err, duration) {
+        if (err) return console.log(err.message);
+        files.push({
+          title: f.name,
+          torrent: torrent.infoHash,
+          index: torrent.files.indexOf(f),
+          duration: duration
+        })
+        callback();
       })
+   }, function(err) {
+      console.log('last callback');
+      if (err) console.log(err.message)
+      event.sender.send('update-song', files);
     })
-    event.sender.send('update-song', files)
   })
 })
 
@@ -25,9 +36,9 @@ ipcMain.on('play', (event, args) => {
     server = client.get(args[0]).createServer();
     server.listen(9999);
   }
-  event.sender.send('can-play', args[1]);
+  event.sender.send('can-play', [args[1], args[2]]);
 })
 
 ipcMain.on('open-file-explorer', (event, args) => {
-  
+
 })
