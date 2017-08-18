19dc4d09ac5f8ce303c3787dfd2b6887bf8da2c7
Rafael <rafaelleru95103@gmail.com>
2017-08-19 00:56:17 +0200

    title to player, new args

--- ipcRenderer.js
+++ ipcRenderer.js
@@ -1,7 +1,8 @@
 var audio_tag = document.getElementById('audio')
 
 ipcRenderer.on('can-play', (event, args) => {
-  audio_tag.src = 'http://localhost:9999/' + args.toString();
+  audio_tag.src = 'http://localhost:9999/' + args[0].toString();
+  audio_tag.title = args[1];
 })
 
 var open_file_explorer = function() {
